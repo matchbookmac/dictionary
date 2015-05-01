94fd10a55c077942577107b6ba65d587e0b42a35
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-01 11:03:36 -0700

    Add first integration spec and css.

---
+++ spec/definition_integration_spec.rb
@@ -0,0 +1,38 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the dictionary path', {:type => :feature}) do
+  it('will show the user the homepage') do
+    visit('/')
+    expect(page).to have_content('My Personal Dictionary')
+  end
+
+  it('will let the user add a word and navigate home') do
+    visit('/')
+    click_button('Add Word')
+    fill_in('word', :with => 'Ruby')
+    fill_in('type', :with => 'Noun')
+    fill_in('definition', :with => 'An elegant and versatile programming language')
+    click_button('Add')
+    expect(page).to have_content('Your word: Ruby has been added to the dictionary')
+    click_button('Home')
+    expect(page).to have_content('My Personal Dictionary')
+    expect(page).to have_content('Ruby')
+  end
+
+  # it('will let the user add a word and then adding another definition to the word') do
+  #   visit('/')
+  #   click_button('Add Word')
+  #   fill_in('word', :with => 'Ruby')
+  #   fill_in('type', :with => 'Noun')
+  #   fill_in('definition', :with => 'An elegant and versatile programming language')
+  #   click_button('Add')
+  #   expect(page).to have_content('Your word: Ruby has been added to the dictionary')
+  #   click_button('Home')
+  #   expect(page).to have_content('My Personal Dictionary')
+  #   expect(page).to have_content('Ruby')
+  # end
+
+end
