dab537a9a09dba4dcfb2ef7d04f9b6bccd5f792f
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-01 10:23:45 -0700

    Add unique id and method to find word from that id to Word class.

--- lib/word.rb
+++ lib/word.rb
@@ -1,9 +1,10 @@
 class Word
-  attr_reader(:spelling)
+  attr_reader(:spelling, :id)
   @@words = []
 
   define_method(:initialize) do |attributes|
     @spelling = attributes.fetch(:spelling)
+    @id = @@words.length().+(1)
   end
 
   define_singleton_method(:all) do
@@ -16,5 +17,15 @@ class Word
 
   define_singleton_method(:clear) do
     @@words = []
+  end
+
+  define_singleton_method(:find) do |id|
+    found_word = nil
+      @@words.each() do |word|
+        if(word.id().eql?(id))
+          found_word = word
+        end
+      end
+    found_word
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,6 +3,10 @@ require('definition')
 require('word')
 
 describe(Word) do
+  before() do
+    Word.clear()
+  end
+
   describe('#spelling') do
     it('returns the spelling of the word') do
       test_word = Word.new({:spelling => 'Ruby'})
@@ -33,6 +37,23 @@ describe(Word) do
     end
   end
 
+  describe('#id') do
+    it('returns the unique id of a given word') do
+      test_word = Word.new({:spelling => 'Ruby'})
+      test_word.save()
+      expect(test_word.id()).to(eq(1))
+    end
+  end
+
+  describe('.find') do
+    it('returns a word given its unique id') do
+      test_word = Word.new({:spelling => 'Ruby'})
+      test_word.save()
+      test_word_2 = Word.new({:spelling => 'Ruby'})
+      test_word_2.save()
+      expect(Word.find(2)).to(eq(test_word_2))
+    end
+  end
   # describe('#add_definition') do
   #   it('adds a definition to the word') do
   #     test_definition = Definition.new({:type => 'noun', :definition => 'A programming language.'})
