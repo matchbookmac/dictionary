2cfc85dac0ab06de652a29713a93ccbbbf4fde1c
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-01 15:14:34 -0700

    Fix indentation and spacing in app and definition spec.

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
 require('./lib/definition')
-require('pry')
 also_reload('lib/**/*.rb')
 
 get('/') do
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,7 +2,6 @@ require('rspec')
 require('definition')
 
 describe(Definition) do
-
   describe('#definition') do
     it('returns the definition of the definition object') do
       test_definition = Definition.new({:type => 'noun', :definition => 'A programming language.'})
