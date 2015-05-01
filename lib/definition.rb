5460e3760d521262053e36fbcf6f5ae189d78db4
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-01 09:48:46 -0700

    Add all singleton method to Definition class.

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,12 +1,13 @@
 class Definition
   attr_reader(:type, :definition)
+  @@definitions = []
 
   define_method(:initialize) do |attributes|
     @type = attributes.fetch(:type)
     @definition = attributes.fetch(:definition)
   end
 
-  define_method(:all) do
-
+  define_singleton_method(:all) do
+    @@definitions
   end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -20,5 +20,14 @@ describe(Definition) do
     it('returns an empty array of definitions') do
       expect(Definition.all()).to(eq([]))
     end
+  end
 
+  describe('.clear') do
+    it('clears the array of definitions') do
+      test_definition = Definition.new({:type => 'noun', :definition => 'A programming language.'})
+      test_definition.save()
+      Definition.clear()
+      expect(Definition.all()).to(eq([]))
+    end
+  end
 end
