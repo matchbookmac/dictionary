b2138acb1d1b96da543277b00e90aeee5e866dbe
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-01 10:19:19 -0700

    Add all, save, and clear methods to Word class.

--- lib/word.rb
+++ lib/word.rb
@@ -1,6 +1,20 @@
 class Word
   attr_reader(:spelling)
+  @@words = []
+
   define_method(:initialize) do |attributes|
     @spelling = attributes.fetch(:spelling)
+  end
+
+  define_singleton_method(:all) do
+    @@words
+  end
+
+  define_method(:save) do
+    @@words.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@words = []
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -10,6 +10,38 @@ describe(Word) do
     end
   end
 
+  describe('.all') do
+    it('returns an empty array of words') do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('returns an array of the words') do
+      test_word = Word.new({:spelling => 'Ruby'})
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+
+  describe('.clear') do
+    it('clears the array of words') do
+      test_word = Word.new({:spelling => 'Ruby'})
+      test_word.save()
+      Word.clear()
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  # describe('#add_definition') do
+  #   it('adds a definition to the word') do
+  #     test_definition = Definition.new({:type => 'noun', :definition => 'A programming language.'})
+  #     test_word = Word.new({:spelling => 'Ruby'})
+  #     test_word.add_definition(test_definition)
+  #     expect(test_word.define()).to(eq(test_definition))
+  #   end
+  # end
+  #
   # describe('#define') do
   #   it('returns the definition of a word') do
   #     test_definition = Definition.new({:type => 'noun', :definition => 'A programming language.'})
